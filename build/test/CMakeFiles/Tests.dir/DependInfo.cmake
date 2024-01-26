dd0b7c59786f417ef71587068ea2c4c4164be5ff
Ani Dermendzhieva <130653510+AnnieeMD@users.noreply.github.com>
2024-01-26 12:10:54 +0200

    Add files via upload

---
+++ build/test/CMakeFiles/Tests.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/HW/src/atp/ATPElement.cpp" "test/CMakeFiles/Tests.dir/__/src/atp/ATPElement.cpp.obj" "gcc" "test/CMakeFiles/Tests.dir/__/src/atp/ATPElement.cpp.obj.d"
+  "D:/HW/src/atp/ATPTreeGenerator.cpp" "test/CMakeFiles/Tests.dir/__/src/atp/ATPTreeGenerator.cpp.obj" "gcc" "test/CMakeFiles/Tests.dir/__/src/atp/ATPTreeGenerator.cpp.obj.d"
+  "D:/HW/src/atp/Commands.cpp" "test/CMakeFiles/Tests.dir/__/src/atp/Commands.cpp.obj" "gcc" "test/CMakeFiles/Tests.dir/__/src/atp/Commands.cpp.obj.d"
+  "D:/HW/src/atp/Runner.cpp" "test/CMakeFiles/Tests.dir/__/src/atp/Runner.cpp.obj" "gcc" "test/CMakeFiles/Tests.dir/__/src/atp/Runner.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/test/CMakeFiles/Tests.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tests.dir/__/src/atp/ATPElement.cpp.obj"
+  "CMakeFiles/Tests.dir/__/src/atp/ATPElement.cpp.obj.d"
+  "CMakeFiles/Tests.dir/__/src/atp/ATPTreeGenerator.cpp.obj"
+  "CMakeFiles/Tests.dir/__/src/atp/ATPTreeGenerator.cpp.obj.d"
+  "CMakeFiles/Tests.dir/__/src/atp/Commands.cpp.obj"
+  "CMakeFiles/Tests.dir/__/src/atp/Commands.cpp.obj.d"
+  "CMakeFiles/Tests.dir/__/src/atp/Runner.cpp.obj"
+  "CMakeFiles/Tests.dir/__/src/atp/Runner.cpp.obj.d"
+  "Tests.exe"
+  "Tests.exe.manifest"
+  "Tests.pdb"
+  "libTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
